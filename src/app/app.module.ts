0ae663a3bea3ab7f83f248906bef08bb36db2056
SeanCarlson96 <carlsonseanr@gmail.com>
2023-07-19 19:29:35 -0500

    set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroComponentComponent } from './hero-component/hero-component.component';
+import { PreviewComponentComponent } from './preview-component/preview-component.component';
+import { CheckoutComponentComponent } from './checkout-component/checkout-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponentComponent,
+    PreviewComponentComponent,
+    CheckoutComponentComponent
   ],
   imports: [
     BrowserModule
